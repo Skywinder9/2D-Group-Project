ea7dd5afe3204b7fbe53d84abdef691478fb758f
Skywinder9 <Skywinder9@gmail.com>
2015-04-02 17:23:03 -0400

    enemy changes
    we got the enemy to hit a fellow enemy and switch directions.

--- Assets/Scripts/EnemyGuideWatcher.cs
+++ Assets/Scripts/EnemyGuideWatcher.cs
@@ -4,12 +4,31 @@ using System.Collections;
 public class EnemyGuideWatcher : MonoBehaviour
 {
 	public EnemyControllerScript enemyObject = null;
-	
+	public bool Touched = false;
 	void OnTriggerExit2D( Collider2D otherObj )
 	{
 		// If this trigger just left a Platform object, then the enemy 
 		// is about to walk off the platform. Tell the enemy that they need to switch directions!
-		if(otherObj.tag == "Platform")
-			enemyObject.switchDirections();
+
+
+		if (otherObj.tag == "Platform") 
+		{
+			enemyObject.switchDirections ();
+			Touched = false;
+		}
+
+	}
+	void OnTriggerEnter2D( Collider2D otherObj )
+	{
+		// If this trigger just left a Platform object, then the enemy 
+		// is about to walk off the platform. Tell the enemy that they need to switch directions!
+		
+		
+		if (otherObj.tag == "Enemy" && !Touched) 
+		{
+			enemyObject.switchDirections ();
+			Touched = true;
+		}
+		
 	}
 }
